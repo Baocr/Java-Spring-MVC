f13b7e74598a120b321e36bed9eff2b0f435c92f
Bao Nguyen <quocbao0651@gmail.com>
2025-06-17 19:11:17 +0700

    newUser

--- 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,38 +1,52 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
-public class UserController {
+// Annotation này đánh dấu lớp là một Controller trong Spring, giúp Spring quản lý các yêu cầu HTTP đến lớp này
 
-    private UserService userService;
-    
-    public UserController(UserService userService) {
-        this.userService = userService;
+public class UserController 
+// Đây là lớp dùng để xử lý các yêu cầu HTTP liên quan đến người dùng
+{
+
+    private UserService userService; 
+    // // Đây là một biến thành viên của lớp, 
+    // dùng để lưu trữ đối tượng UserService, 
+    // giúp xử lý logic nghiệp vụ liên quan đến người dùng
+
+    public UserController(UserService userService) // Constructor của lớp,
+    {
+        this.userService = userService; // Gán đối tượng UserService vào biến thành viên
     }
 
-    @RequestMapping("/")
-    public String getHomePage() {
-        return "eric.html";
+    @RequestMapping("/") // Annotation này đánh dấu method này sẽ xử lý các yêu cầu HTTP đến đường dẫn gốc ("/")
+    // Method này sẽ trả về tên của trang HTML sẽ được hiển thị khi người dùng truy cập vào đường dẫn gốc
+    public String getHomePage(Model model) // Method này dùng để xử lý yêu cầu HTTP đến đường dẫn gốc
+    {
+        String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        return "hello"; // Trả về tên của trang HTML sẽ được hiển thị,
+    }
+
+    @RequestMapping("/admin/user") // Annotation này đánh dấu method này sẽ xử lý các yêu cầu HTTP đến đường dẫn "/user"
+    public String userPage(Model model) // Method này dùng để xử lý yêu cầu HTTP đến đường dẫn gốc
+    {
+        model.addAttribute("newUser", new User());
+        return "/admin/user/create";
+    }
+
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoibaoUser) {
+        System.out.println("Run here"+ hoibaoUser);
+        return "hello";
     }
 }
-
-// @RestController
-// public class UserController {
-
-//     private UserService userService;
     
-
-//     public UserController(UserService userService) {
-//         this.userService = userService;
-//     }
-
-
-//     @GetMapping("")
-//     public String getHomePage() {
-//         return this.userService.handleHello();
-//     }
-// }
--- 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -2,9 +2,12 @@ package vn.hoidanit.laptopshop.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
-    public String handleHello() {
-        return "Hello from UserService";
+@Service // Annotation này đánh dấu lớp là một Service trong Spring, giúp Spring quản lý bean của lớp này
+
+public class UserService // Đây là lớp dùng để xử lý logic nghiệp vụ liên quan đến người dùng
+{
+    public String handleHello() // Method này dùng để trả về một chuỗi đơn giản, có thể là thông điệp chào mừng hoặc thông tin khác
+    {
+        return "Hello from UserService!"; // Trả về chuỗi "Hello from UserService!"
     }
 }
